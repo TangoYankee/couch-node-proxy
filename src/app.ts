10e9699b10d3d306708197f0cab327ced708965e
tangoyankee <miller.tim108@gmail.com>
2020-06-17 16:58:02 -0700

    add route for cars

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { HttpRequest } from './http-request/http-request'
 import { ServerResponse } from 'http'
 
 export const requestHandler = async (req: http.IncomingMessage, res: ServerResponse) => {
+  console.log(req.url)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
@@ -13,7 +14,12 @@ export const requestHandler = async (req: http.IncomingMessage, res: ServerRespo
       const httpRequest = new HttpRequest(url, req.method!)
       const body: any = await httpRequest.getRequest()
       res.end(body)
-    } else {
+    } else if (url === '/cars/'){
+      console.log('hit cars')
+      const httpRequest = new HttpRequest(url, req.method!)
+      const body: any = await httpRequest.getRequest()
+      res.end(body)
+    }else {
       res.statusCode = 401
       res.end('{"message": "go away"}')
     }
