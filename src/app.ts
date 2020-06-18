e48c87bdb5222dc97d0734cb86878727e673978d
tangoyankee <miller.tim108@gmail.com>
2020-06-18 11:03:31 -0700

    configure proxy server to handle basic auth and handle errors

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,19 @@ import { HttpRequest } from './http-request/http-request'
 import { ServerResponse } from 'http'
 
 export const requestHandler = async (req: http.IncomingMessage, res: ServerResponse) => {
+  console.log(req.url)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   try {
     const url: string = req.url!
     if (url === '/') {
+      const httpRequest = new HttpRequest(url, req.method!)
+      const body: any = await httpRequest.getRequest()
+      res.end(body)
+    } else if (url === '/cars/') {
+      console.log('hit cars')
       const httpRequest = new HttpRequest(url, req.method!)
       const body: any = await httpRequest.getRequest()
       res.end(body)
@@ -25,6 +31,45 @@ export const requestHandler = async (req: http.IncomingMessage, res: ServerRespo
 
 const authServer = http.createServer(requestHandler)
 
-authServer.listen(8000, () => {
-  console.log('server started on port 8000')
+// authServer.listen(8000, () => {
+//   console.log('server started on port 8000')
+// })
+
+const couchDomain = 'localhost'
+const couchPort = 5984
+
+const proxyHandler = async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
+
+  var auth: string = req.headers.authorization ? 'creds' : 'anon'
+  var proxyReqHeaders = JSON.parse(JSON.stringify(req.headers))
+  proxyReqHeaders.host = `${couchDomain}:${couchPort}`
+  proxyReqHeaders.origin = 'http://localhost:3000'
+  // proxyReqHeaders.setHeader('Forwarded', 'for')
+  console.log('proxy headers')
+  console.log(proxyReqHeaders)
+  var options = {
+    // host: couchDomain,
+    port: couchPort,
+    headers: proxyReqHeaders,
+    method: JSON.parse(JSON.stringify(req.method)),
+    path: JSON.parse(JSON.stringify(req.url))
+  }
+  var body:any
+  try {
+    body = await HttpRequest.getRequest(options)
+    console.info(body)
+  } catch (error) {
+    body = JSON.stringify({error: error.message})
+    console.info(body)
+  } finally{
+    res.end(body)
+  }
+}
+
+const proxyServer = http.createServer(proxyHandler)
+
+proxyServer.listen(3000, () => {
+  console.log('proxy server started on port 3000')
 })
--- src/http-request/http-request.ts
+++ src/http-request/http-request.ts
@@ -29,10 +29,6 @@ export class HttpRequest {
   public static getRequest (options: http.RequestOptions, postData?: string) {
     return new Promise((resolve, reject) => {
       var req = http.request((options), (res: http.IncomingMessage) => {
-        const statusCode: number = res.statusCode!
-        if (statusCode < 200 || statusCode >= 300) {
-          return reject(new Error(`statusCode=${res.statusCode!}`))
-        }
         var body: Array<Uint8Array> = []
         res.on('data', (chunk: Uint8Array) => {
           body.push(chunk)
